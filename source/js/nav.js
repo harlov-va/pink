7bbb05eab19dd92b1f8b43032bb7ea21ee9739ce
harlov-va <harlov-va@gmail.com>
2021-12-23 09:52:22 +0300

    I've been written the layout of the stors block

--- source/js/nav.js
+++ source/js/nav.js
@@ -7,3 +7,17 @@ const resize = () => {
 }
 
 window.addEventListener('resize', _.debounce(resize, 100));
+
+const header = document.querySelector('.page-header');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+header.classList.remove('page-header--nojs');
+navToggle.addEventListener('click', function() {
+  if (header.classList.contains('page-header--nav-closed')) {
+    header.classList.remove('page-header--nav-closed');
+    header.classList.add('page-header--nav-opened');
+  } else {
+    header.classList.remove('page-header--nav-opened');
+    header.classList.add('page-header--nav-closed');
+  }
+});
