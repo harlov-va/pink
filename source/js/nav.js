ce137f385101daee8b301967257b322476bcfdac
Your Name <harlov.va@gmail.com>
2022-05-15 12:42:39 +0300

    I've added Google map.

--- source/js/nav.js
+++ source/js/nav.js
@@ -10,8 +10,11 @@ window.addEventListener('resize', _.debounce(resize, 100));
 
 const header = document.querySelector('.page-header');
 const navToggle = document.querySelector('.main-nav__toggle');
+const contactsMap = document.querySelector('.contacts__map-wrapper');
 
 header.classList.remove('page-header--nojs');
+contactsMap.classList.remove('contacts__map-wrapper--nojs')
+
 navToggle.addEventListener('click', function() {
   if (header.classList.contains('page-header--nav-closed')) {
     header.classList.remove('page-header--nav-closed');
